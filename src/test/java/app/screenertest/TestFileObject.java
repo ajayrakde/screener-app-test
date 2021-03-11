5b209cac59bb8b253f6d3d1c35fe1a20475a3879
ajayrakde <rakadeajay@gmail.com>
2021-03-11 15:23:44 +0530

    Add files via upload

---
+++ src/test/java/app/screenertest/TestFileObject.java
@@ -0,0 +1,23 @@
+package app.screenertest.datarepo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.jsonschema.JsonSerializableSchema;
+
+@JsonSerializableSchema
+public class TestFileObject {
+  
+    @JsonProperty
+    String launchurl;
+	
+	@JsonProperty
+    TestObject[] testarray;
+    
+    
+	public String getLaunchUrl() {
+		return this.launchurl;
+	}
+	public TestObject[] getTestarray() {
+        return testarray;
+    }
+}
+
