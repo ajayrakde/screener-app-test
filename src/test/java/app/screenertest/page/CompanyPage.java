4c4f7d63b7fdeabd9f2640537b9d0f0a17b206de
ajayrakde <rakadeajay@gmail.com>
2021-05-08 22:52:19 +0530

    Add files via upload

---
+++ src/test/java/app/screenertest/page/CompanyPage.java
@@ -0,0 +1,43 @@
+package app.screenertest.page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import app.screenertest.datamanager.FileStorageManager;
+
+public class CompanyPage {
+
+	WebDriver driver;
+	
+	public CompanyPage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public WebDriver downloadExcel(String opfolderpath, String ISIN) 
+	{
+		int dload=0;
+		try {
+			dload =new FileStorageManager().getCountOfFilesFromPath(opfolderpath,".xlsx");   
+			driver.get("https://www.screener.in/company/"+ISIN+"/");
+			WebDriverWait wait = new WebDriverWait(driver, 5);
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='top']/div/div/button/span")));
+			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='top']/div/div/button/span")));
+			driver.findElement(By.xpath("//div[@id='top']/div/div/button/span")).click();
+			while(new FileStorageManager().getCountOfFilesFromPath(opfolderpath,".xlsx")!=dload+1)
+			{
+				Thread.sleep(1000);
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		return this.driver;
+	}
+	
+}
